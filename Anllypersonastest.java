f9df3ba7eba22b84e713bb27b50f47d97d507d66
AnllyAnalyCastilloVilleda <110704559+AnllyAnalyCastilloVilleda@users.noreply.github.com>
2022-08-06 19:59:10 -0600

    Add files via upload

---
+++ Anllypersonastest.java
@@ -0,0 +1,12 @@
+
+package personas;
+
+public class PersonasTest {
+   public static void main(String [] args){
+       Persona p1 = new Persona("Fabricio","Mejia","1094","2004");
+       Persona p2 = new Persona("Anlly","Castillo","3575","2003");
+       p1.DepliegaInfo();
+       p2.DepliegaInfo();
+      
+   } 
+}
